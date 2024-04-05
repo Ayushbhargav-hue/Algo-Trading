97d48251700a13ff5ee6e7251e786ce4ca94030e
PranPakheru <pranav.shahu.ps@gmail.com>
2024-04-05 14:30:07 +0530

    first commit by Pranav Shahu, responsive dashboard

---
+++ Script.js
@@ -0,0 +1,20 @@
+
+function navigateToAboutUs(){
+    window.location.href = "aboutUs.html";
+}
+
+function navigateToStocks(){
+    window.location.href = "stocks.html";
+}
+
+function navigateToAlgos(){
+    window.location.href = "algos.html";
+}
+
+function navigateToProfile(){
+    window.location.href = "profile.html";
+}
+
+function navigateToLogout(){
+    window.location.href = "logout.html";
+}
